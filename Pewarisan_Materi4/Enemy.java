553bea0faa83f9230ec8d3b00c9e4cd282457efc
DyanaSarahP <162670507+DyanaSarahP@users.noreply.github.com>
2025-01-13 21:29:48 +0700

    Add files via upload

---
+++ Pewarisan_Materi4/Enemy.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pewarisan_Materi4;
+
+/**
+ *
+ * @author HP
+ */
+public class Enemy {
+    String name = "Dyana";
+     int Hp = 23;
+     int attackPoin = 85;
+     
+     void attack(){
+         System.out.println("Serang!");
+     }
+    
+    
+}
---
+++ Pewarisan_Materi4/Pocong.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pewarisan_Materi4;
+
+/**
+ *
+ * @author HP
+ */
+public class Pocong extends Enemy{
+     void jump(){
+        System.out.println("Pocong Loncat-Loncat");
+    }
+    void attack(){
+        System.out.println("Serang Pocong !!!!!");
+    }
+}
